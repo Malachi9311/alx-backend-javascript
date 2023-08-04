d1950dd981efdff6afa5b80e66b30b127b433ae8
Malachi9311 <ntuthuko93dlamini@gmail.com>
2023-08-04 15:02:24 +0200

    update

--- 0x03-ES6_data_manipulation/4-update_grade_by_city.js
+++ 0x03-ES6_data_manipulation/4-update_grade_by_city.js
@@ -1,22 +1,14 @@
-export default function updateStudentGradeByCity(students, city, newGrades) {
-  if (!Array.isArray(students) || !Array.isArray(newGrades)) {
+const updateStudentGradeByCity = (studentList, city, grades) => {
+  if (!Array.isArray(studentList) || !Array.isArray(grades)) {
     return [];
   }
-  // Filter students by location
-  // Change/add grade for students in newGrades
-  // If a filtered student is not in new Grades => grade = N/A
-
-  const cityStudents = students.filter((student) => student.location === city).map((student) => {
-    const item2 = newGrades.find((student2) => student.id === student2.studentId);
-    // });
-    // console.log('FOUND STUDENT1: ', student);
-    // console.log('FOUND ITEM2: ', item2);
-    // console.log(item2 ? {...student, ...item2} : student.grade = 'N/A', student);
-    // if (!item2) {
-    //   student.grade = 'N/A'; // eslint-disable-line no-param-reassign
-    // }
-    return { ...student, grade: item2 ? item2.grade : 'N/A' };
-    // return item2 ? {...student, ...item2} : student,  student.grade = 'M/A';
-  });
+  const cityStudents = studentList
+    .filter((student) => student.location === city)
+    .map((student) => {
+      const newArr = grades.find((student2) => student.id === student2.studentId);
+      return { ...student, grade: newArr ? newArr.grade : 'N/A' };
+    });
   return cityStudents;
-}
+};
+
+export default updateStudentGradeByCity;
